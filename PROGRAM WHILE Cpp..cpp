55769c633fe7a0506a4ac18e0f25647a5239ac63
Azis Maulana <45529501+Azis24@users.noreply.github.com>
2018-12-08 20:19:57 +0700

    Add files via upload

---
+++ PROGRAM WHILE Cpp..cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	system ("color E");
+	int x=1;
+	while (x<100) //the condisionalnya "x<10"
+	{
+	    ++x; // stetmenya
+	    cout<<"Masukan Angkanya = "<<x<<endl;
+	}
+}
